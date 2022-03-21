08066c720a7b19cd91a4c93a4026b5044a9a1eee
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-03-21 14:34:11 +0100

    Create the addBookHandler function

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const addBtn = document.getElementById('add-btn');
 
-
 const arrayBooks = [];
-let objectBook = {}; 
 
-function addBook(title, author) {
-    objectBook.title = title
-    objectBook.author = author;
-    arrayBooks.push(objectBook);
-    console.log(arrayBooks); 
-}
 
-addBtn.addEventListener('click', (event) => {
+// Create the Books array
+const addBookHandler = () => {
     const bookTitle = document.getElementById('title').value;
     const bookAuthor = document.getElementById('author').value;
-    event.preventDefault()
-    addBook(bookTitle, bookAuthor)
-})
+    
+    if (bookTitle.trim() === '' || bookAuthor.trim() === '') {
+        return;
+    }
+
+    let objectBook = {
+        bookTitle,
+        bookAuthor
+    };
+
+    arrayBooks.push(objectBook);
+    console.log(arrayBooks);
+}
+
+addBtn.addEventListener('click', addBookHandler)
