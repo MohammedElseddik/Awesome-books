35edea412abc3d74428b0e568390e29b1f624de2
dfizzkruah93 <dfizzkruah@gmail.com>
2022-03-21 18:39:02 +0200

    Update book list to the UI

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const addBtn = document.getElementById('add-btn');
+const bookListUi = document.querySelector('.book-list-ui');
 
 const arrayBooks = [];
 
+const removeBookHandler = () => {
+
+};
+
+// Create a render function
+const renderBook = () => {
+    const listUi = document.createElement('ul');
+    listUi.setAttribute('class', 'list-ui');
+    const removeBtn = document.createElement('button');
+    removeBtn.setAttribute('class', 'remove-btn');
+    removeBtn.textContent = 'Remove';
+
+    // check length add remove class visible
+
+    
+   
+        
+        for (let i=arrayBooks.length; i<=arrayBooks.length;) {
+            for(const key in arrayBooks[i]) {
+                const bookListElement = document.createElement('li');
+                bookListElement.textContent = arrayBooks[i][key];      
+                listUi.appendChild(bookListElement);
+            }  
+            i = arrayBooks.length;
+        }
+
+        listUi.appendChild(removeBtn); 
+        bookListUi.appendChild(listUi);   
+
+    
+};
 
 // Create the Books array
 const addBookHandler = () => {
@@ -19,6 +51,8 @@ const addBookHandler = () => {
 
     arrayBooks.push(objectBook);
     console.log(arrayBooks);
+    renderBook();
 }
 
-addBtn.addEventListener('click', addBookHandler)
+addBtn.addEventListener('click', addBookHandler);
+// removeBtn.addEventListener('click', removeBookHandler);
